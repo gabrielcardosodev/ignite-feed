c505f3768d10613b29fa11b72d728e2892601394
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-06-16 18:14:00 -0300

    add header component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Header } from "./components/Header";
+
+import './global.css'
+
+export function App() {
+  
+  return (
+    <div>
+      <Header />
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import styles from './Header.module.css'
+
+import logoImg from '../assets/images/logo.svg'
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={logoImg} alt="logo do ignite feed" />
+      <strong className={styles.strong}>Ignite Feed</strong>
+    </header>
+  )
+}
