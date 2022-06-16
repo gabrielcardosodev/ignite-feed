bb7aea517750f5b85da25ba8f1cf27084bd06cae
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-06-16 20:34:31 -0300

    some changes in sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,16 @@
 import styles from './Sidebar.module.css'
 
 import pencilImg from '../assets/images/pencil.svg'
+import profilePic1 from '../assets/images/profile-pic-1.png'
+import coverImg from '../assets/images/cover.png'
 
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
-      <img className={styles.cover} src="https://images.unsplash.com/photo-1653744003874-4d2424bf85e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
+      <img className={styles.cover} src={coverImg} />
 
       <div className={styles.profile}>
-        <img src="https://avatars.githubusercontent.com/u/83722145?v=4" />
+        <img src={profilePic1} />
         <strong>
           Gabriel Cardoso
         </strong>
@@ -23,7 +25,7 @@ export function Sidebar() {
         <path d="M5.50001 14.875H1.75C1.58424 14.875 1.42527 14.8091 1.30806 14.6919C1.19085 14.5747 1.125 14.4158 1.125 14.25V10.7578C1.12472 10.6766 1.14044 10.5962 1.17128 10.5212C1.20211 10.4461 1.24745 10.3778 1.30469 10.3203L10.6797 0.945305C10.7379 0.886252 10.8072 0.839356 10.8836 0.807345C10.9601 0.775335 11.0421 0.75885 11.125 0.75885C11.2079 0.75885 11.2899 0.775335 11.3664 0.807345C11.4428 0.839356 11.5122 0.886252 11.5703 0.945305L15.0547 4.42968C15.1138 4.48784 15.1606 4.55716 15.1927 4.63361C15.2247 4.71006 15.2412 4.79211 15.2412 4.87499C15.2412 4.95787 15.2247 5.03993 15.1927 5.11638C15.1606 5.19283 15.1138 5.26215 15.0547 5.3203L5.50001 14.875ZM5.50001 14.875H14.875M8.625 2.99999L13 7.37499" stroke="#2B369E" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
 
-        Editar perfil
+        Editar seu perfil
         </a>
       </footer>
     </aside>
