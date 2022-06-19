403459b3cb0cbcc421d3d28288e00bba335abf7b
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-06-18 23:29:59 -0300

    feat: add a key to map return in post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -57,10 +57,10 @@ export function Post({ author, content, publishedAt }) {
     <div className={styles.content}>
       {content.map(line => {
         if (line.type === 'paragraph') {
-          return <p>{line.content}</p>
+          return <p key={line.content}>{line.content}</p>
         } else if (line.type === 'link') {
           return (
-          <p>
+          <p key={line.content}>
             <a href={line.content} target='blank'>
               {line.content}
             </a>
@@ -86,7 +86,7 @@ export function Post({ author, content, publishedAt }) {
 
     <div className={styles.commentList}>
       {comments.map(comment => {
-        return <Comment content={comment}/>
+        return <Comment key={comment} content={comment}/>
       })}
     </div>
    </article>
