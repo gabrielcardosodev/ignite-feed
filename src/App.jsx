6a8e462e9778f34f718194147f5d6e9c513a8112
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-06-18 22:08:19 -0300

    feat: create posts array

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,38 @@ import { Sidebar } from "./components/Sidebar";
 import './global.css'
 import styles from './App.module.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/35928107?v=4',
+      name: 'Kayo Oliveira',
+      role: 'Fullstack Developer | LifeVet'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'https://github.com/kayooliveira' },
+    ],
+    publishedAt: new Date('2022-06-18 19:00:00')
+  },
+
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/6643122?v=4',
+      name: 'Mayk Britto',
+      role: 'Sênior Web Dev & Instructor | Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'https://github.com/maykbrito' },
+    ],
+    publishedAt: new Date('2022-06-18 20:00:00'),
+  }
+]
+
 export function App() {
   
   return (
@@ -15,8 +47,16 @@ export function App() {
         <Sidebar />
 
         <main>
-          <Post />
-          <Post />
+         {posts.map(post => {
+          return (
+            <Post
+              key={post.id}
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          )
+         })}
         </main>
       </div>
     </div>
