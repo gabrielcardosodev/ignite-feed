74842cdefe38006594ed7897dc32bb39c0061549
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-06-19 03:10:27 -0300

    feat: add functionality for add likes in comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import { Avatar } from './Avatar'
 
 import { ThumbsUp, Trash } from 'phosphor-react'
@@ -7,8 +9,14 @@ import styles from './Comment.module.css'
 
 
 export function Comment({ content, onDeleteComment }) {
+  const [ likeCount, setLikeCount ] = useState(0)
+
   function handleDeleteComment() {
     onDeleteComment(content)
+  }
+ 
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1)
   }
 
   return (
@@ -32,9 +40,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp size={20}/>
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
